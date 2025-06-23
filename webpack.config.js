0b685a82d752ebafcd80c8de314e7af7d32f2e24
Robert <comps@diprobe.com>
2025-06-23 10:15:34 +0200

    webpack plugin use

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const TerserPlugin = require("terser-webpack-plugin");
+const CopyRenameWebpackPlugin = require("copy-rename-webpack-plugin");
 
 module.exports = {
   mode: 'production',
